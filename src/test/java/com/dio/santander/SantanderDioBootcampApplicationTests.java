33ab809d059b9dd0d0e38ee933657bb553a6b09c
Lara Marinho Cordeiro <laramaricordeiro@gmail.com>
2025-03-12 18:16:41 -0300

    First commit

---
+++ src/test/java/com/dio/santander/SantanderDioBootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.santander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDioBootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
